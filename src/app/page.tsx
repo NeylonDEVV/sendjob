3352d6dc8d932658d92c81c395907421909974d2
NeylonDEV <neylondev@gmail.com>
2024-08-31 17:52:51 -0300

    correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import HomeServer from "./Home/page";
-import scheduleEmail from "./server/enviar-curriculo";
-
 
 export default function Home() {
-  scheduleEmail();
-
   return (
     <div className="flex flex-col p-20 items-center justify-center">
       <HomeServer />
